8a96b6ee5ad0bdc35369dec3baf989fa5892ba5b
Leather-Face619 <rahul852369hhhhh@gmail.com>
2024-09-04 11:39:19 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 
-import { Environment, OrbitControls } from '@react-three/drei'
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import MacContainer from './MacContainer'
 const App = () => {
   return (
-    <Canvas camera={{fov:12, position:[0, -10, 120]}}>
+    <Canvas camera={{fov:12, position:[0, -10, 220]}}>
       <OrbitControls/>
 
       <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/rosendal_plains_2_4k.exr"]}/>
-     <MacContainer/>
+      <ScrollControls pages={3}>
+<MacContainer/>
+      </ScrollControls>
     </Canvas>
   )
 }
--- src/MacContainer.jsx
+++ src/MacContainer.jsx
@@ -1,14 +1,35 @@
-import { useGLTF } from "@react-three/drei"
+import { useGLTF, useScroll, useTexture } from "@react-three/drei"
+import { useFrame } from "@react-three/fiber"
 
+import * as THREE from "three"
 const MacContainer = () => {
+let model = useGLTF("./mac.glb") // Make sure this is a .gltf or .glb file
+let tex = useTexture("./f.png")
+let meshes ={}
+model.scene.traverse((e) => {
+  meshes[e.name] = e
+})
+// console.log(meshes);
+ 
+meshes.screen.rotation.x = THREE.MathUtils.degToRad(180.2)
+meshes.screen.scale.x =1.2
+meshes.body.scale.x =1.2
+meshes.matte.material.map = tex
+meshes.matte.material.emissiveIntensity = 0
+meshes.matte.material.metalness = 1
+meshes.matte.material.roughness = 8
 
-    let model = useGLTF("./mac.glb")
+let data = useScroll()
+useFrame((state,delta)=>{
+meshes.screen.rotation.x = THREE.MathUtils.degToRad(180.2-(data.offset * 90))
+
+})
   return (
 
-<group position={[0,-10,20]}>
-   
-    <primitive object={model.scene}/>
-</group>
+    <group position={[0, -10, 20]}>
+
+      <primitive object={model.scene} />
+    </group>
   )
 }
 
